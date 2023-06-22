a5c7be6a816b74f86bb31260e4c89dc454465128
Bot69 <rupeshacharya2000@gmail.com>
2023-06-22 23:52:59 +0545

    link pages

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,about,service
 urlpatterns = [
-    path('',index),
+    path('',index,name="index"),
+    path('about/',about,name="about-us"),
+    path('service/',service,name="service"),
+    path('contact-us/',about,name="contact-us")
+
+
 ]
--- website/views.py
+++ website/views.py
@@ -5,6 +5,16 @@ from django.shortcuts import render
 def index(request):
     return render(request,'website/index.html')
 
+def about(request):
+    return render(request,'website/about.html')
+
+
+def service(request):
+    return render(request,'website/service.html')
+
+def contact(request):
+    return render(request,'website/contact.html')
+
 
 
 
