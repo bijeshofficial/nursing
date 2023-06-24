d813cc21349e2303b809d337fe6a7fc68c4a2ec7
Bot69 <rupeshacharya2000@gmail.com>
2023-06-24 12:15:22 +0545

    add admin panel

--- administrator/views.py
+++ administrator/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from website.models import JobSeeker
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'administrator/index.html')
+    job_seeker = JobSeeker.objects.all()
+    context ={
+        "job_seeker": job_seeker,
+        "name":"rupesh"
+    }
+    return render(request, 'administrator/index.html',context)
