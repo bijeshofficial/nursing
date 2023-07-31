88aad7a90a2741cf6b92ef64d90868621b991cd7
bijeshofficial <bijeshr.kunwar@gmail.com>
2023-07-31 23:34:52 +0545

    privacy policy added

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, about, service, job_seeker, contact, save_contact,vacancy,training,homecare,disability_support,aged_care,find_jobs,job_detail
+from .views import (index, about, service, job_seeker, contact, 
+                    save_contact,vacancy,training,homecare,
+                    disability_support,aged_care,find_jobs,job_detail,
+                    privacy_policy)
 urlpatterns = [
     path('',index,name="index"),
     # path('home',index, name="home"),
@@ -11,14 +14,10 @@ urlpatterns = [
     path('training/',training,name="training"),
     path('disability-support/',disability_support,name="disability-support"),
     path('aged-care/',aged_care,name="aged-care"),
-    path('contact', contact, name = "contact-us"),
-    path('save-contact', save_contact, name="save-contact"),
-     path('vacancy', vacancy, name="vacancy"),
-    path('find-jobs', find_jobs, name="find-jobs"),
-    path('job/<int:pk>/', job_detail, name="job-detail")
-
-    
-    
-
-
+    path('contact/', contact, name = "contact-us"),
+    path('save-contact/', save_contact, name="save-contact"),
+    path('vacancy/', vacancy, name="vacancy"),
+    path('find-jobs/', find_jobs, name="find-jobs"),
+    path('job/<int:pk>/', job_detail, name="job-detail"),
+    path('privacy-policy/', privacy_policy, name="privacy-policy")
 ]
